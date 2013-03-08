ece0ceb86dca31856bc6982360cbac96b41beb64
Dirk Thomas <dthomas@willowgarage.com>
2013-03-08 08:57:31 -0800

    [xdot] fix execute flag for package bin, remove global bin (fix #1)

--- xdot/setup.py
+++ xdot/setup.py
@@ -5,8 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['xdot'],
-    package_dir={'': 'src'},
-    scripts=['scripts/dot_viewer.py']
+    package_dir={'': 'src'}
 )
 
 setup(**d)
